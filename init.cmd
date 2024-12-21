d1efa8fc2da039f38155085f1e4b7e4a86995e79
kirin123 <kirin123kirin@gmail.com>
2024-12-21 23:53:54 +0900

    submodeles initial updates

--- init.cmd
+++ init.cmd
@@ -1,7 +1,7 @@
 git clone https://github.com/kirin123kirin/.vscode
 git clone https://github.com/kirin123kirin/.github
-rmdir /s /q .vscode/.git
-rmdir /s /q .github/.git
+rmdir /s /q .vscode\.git
+rmdir /s /q .github\.git
 
 git commit -a -m "submodeles initial updates"
 git push
